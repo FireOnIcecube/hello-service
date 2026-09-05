d43b83a9429a0aa539c3d7579534ad3ce65a37d4
FireOnIcecube <fireonicecube@gmail.com>
2026-09-05 22:34:50 +0800

    feat: update TaskRepository interface to use context and models

--- handlers/handler.go
+++ handlers/handler.go
@@ -1,15 +1,40 @@
 package handlers
 
 import (
-	"example.com/hello-service/repositories"
+	"context"
+
+	"example.com/hello-service/models"
 )
 
+type TaskRepository interface {
+	GetAll(
+		ctx context.Context,
+	) ([]models.Task, error)
+
+	Create(
+		ctx context.Context,
+		title string,
+	) (models.Task, error)
+
+	Update(
+		ctx context.Context,
+		id int,
+		title string,
+		completed bool,
+	) (models.Task, error)
+
+	Delete(
+		ctx context.Context,
+		id int,
+	) error
+}
+
 type Handler struct {
-	taskRepository *repositories.TaskRepository
+	taskRepository TaskRepository
 }
 
 func New(
-	taskRepository *repositories.TaskRepository,
+	taskRepository TaskRepository,
 ) *Handler {
 	return &Handler{
 		taskRepository: taskRepository,
